317532bbbe8fd6b2ee9a1ae83924f193808403d6
Elivandro <Elivandro@hotmail.com.br>
2023-09-14 22:27:47 -0400

    models and relations created

---
+++ app/Models/Feature.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Feature extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'unit'
+    ];
+
+    public function feature_sku(): BelongsToMany
+    {
+        return $this->belongsToMany(Feature_sku::class);
+    }
+}
