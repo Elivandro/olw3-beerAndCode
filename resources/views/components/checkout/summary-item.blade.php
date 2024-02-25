93e56c0de54ffea66886bc3346b75d46a3e94bfa
Elivandro <Elivandro@hotmail.com.br>
2024-02-25 15:58:54 -0400

    blade directive money

--- resources/views/components/checkout/summary-item.blade.php
+++ resources/views/components/checkout/summary-item.blade.php
@@ -3,5 +3,5 @@
     'pt-6 text-white border-t border-white border-opacity-10' => $isLast ?? false,
 ])>
     <dt class="text-primary-200">{{ $title }}</dt>
-    <dd class="text-secondary-300">R$ {{ $value }}</dd>
+    <dd class="text-secondary-300">@money($value)</dd>
 </div>
